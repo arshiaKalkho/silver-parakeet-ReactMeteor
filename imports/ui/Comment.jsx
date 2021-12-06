85b369a400a7142668c066e4c9882873e8f22440
Arshia Kalkhorani <arshiabdol@yahoo.com>
2021-12-05 22:39:47 -0800

    added two new components for conversations and comments

---
+++ imports/ui/Comment.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Comment() {
+  return (
+    <p>
+      this is comment
+    </p>
+  )
+}
+
+export default Comment;
---
+++ imports/ui/Conversation.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Comment from './Comment';
+
+export default class Conversation extends React.Component {
+  render() {
+    return (
+      
+        <div>
+          <p> this is conversation</p>
+        <Comment/>
+        </div>
+      
+    )
+  }
+}
